0d9061147f65e931759690604bb645ecbdf7f88b
Silicon Hacks <SiliconHacks@e1z1r5p22.42.us.org>
2017-05-06 22:52:01 -0700

    Created routing

--- server/main.js
+++ server/main.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var interview = require('./Routes/interview');
 var app = express();
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
+
+app.use('/interview', interview);
 
 app.post('/audio', function (req, res) {
 	console.log("RECIEVED AUDIO TO EXTRACT INDICATORS: ", req.body);
@@ -13,4 +15,3 @@ app.post('/audio', function (req, res) {
 app.listen(3000, function() {
 	console.log('App Listening on port ', 3000);
 });
-

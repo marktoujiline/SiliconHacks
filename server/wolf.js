18636b7e9e7475f40e1185a7b4b0df48cefa31b8
Luke <lmunro@calpoly.edu>
2017-05-07 12:20:11 -0700

    wolf

---
+++ server/wolf.js
@@ -0,0 +1,25 @@
+var xml2json = require('xml2js').parseString;
+var request = require('request');
+
+var url = "http://api.wolframalpha.com/v2/query?appid=H77RUP-YKLXJJVT4X&format=plaintext&input=";
+
+
+
+module.exports = function(topic) {
+	return new Promise(function(res, rej) {
+		request(url+topic, function (err, response, body) {
+			console.log('error', err);
+			console.log('statusCode', response && response.statusCode);
+			console.log('body', body);
+			var list = [];
+			xml2json(body, function(err, result) {
+				for (var i = 0; i < result.queryresult.pod.length; i++) {
+					var pod = result.queryresult.pod[i];
+					list.push(pod.subpod[0].plaintext[0]);
+				}
+			// console.log(list);
+			res(list);
+			});
+		});
+	});
+} 

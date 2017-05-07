1aa0e75f2eef3fc99a649304df607b66d4e2c0d4
Duane Irvin <irvin93d@gmail.com>
2017-05-06 22:47:59 -0700

    Receiving audio

--- server/main.js
+++ server/main.js
@@ -3,6 +3,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.raw({ type: 'audio/wav', limit: '50mb' }));
+
+app.post('/audio', function (req, res) {
+	console.log("RECIEVED AUDIO TO EXTRACT INDICATORS: ", req.body);
+	res.status(200).end();
+});
 app.listen(3000, function() {
 	console.log('App Listening on port ', 3000);
 });
